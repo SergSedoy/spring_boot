92c913b20116850752d6525ecad6187254ad1103
SergSedoy <78666743+SergSedoy@users.noreply.github.com>
2021-08-19 17:03:57 +0300

    Update ApplicationTests.java

--- src/test/java/com/example/spring_boot/ApplicationTests.java
+++ src/test/java/com/example/spring_boot/ApplicationTests.java
@@ -3,11 +3,25 @@ package com.example.spring_boot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class DemoApplicationTests {
+    @Autowired
+    TestRestTemplate restTemplate;
+	
+    private static GenericContainer<?> app = new GenericContainer<>("app:1.0");
+    private static GenericContainer<?> appLatest = new GenericContainer<>("app:latest"); 
 
-	@Test
-	void contextLoads() {
-	}
+    @BeforeAll
+    public static void setUp() {
+        app.start();
+	appLatest.start();
+	    
+    }
+
+    @Test
+    void contextLoads() {
+        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" + app.getMappedPort(8080), String.class);
+        System.out.println(forEntity.getBody());
+    }
 
 }
